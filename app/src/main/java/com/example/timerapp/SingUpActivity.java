52a2b1b42f71c0ab8e115cfb3c0880a167a340c5
@farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2022-12-06 01:18:49 +0530

    mvvm testing

--- app/src/main/java/com/example/timerapp/SingUpActivity.java
+++ app/src/main/java/com/example/timerapp/SingUpActivity.java
@@ -3,12 +3,65 @@ package com.example.timerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SingUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    public static final String TAG = "checkStatus";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+    public void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+
+            }else {
+                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                Toast.makeText(SingUpActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        });
+
+
+    }
+    public void updateUI(FirebaseUser firebaseUser){
+
+    }
+    public void SignIn(String email, String password){
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                Toast.makeText(SingUpActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/timerapp/mvvm/LoginViewModel.java
@@ -0,0 +1,8 @@
+package com.example.timerapp.mvvm;
+
+import androidx.databinding.BaseObservable;
+
+
+
+public class LoginViewModel extends BaseObservable {
+}
